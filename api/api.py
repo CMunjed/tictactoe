e2824a322358d1b8e39564b23779584e6b30f4d3
CMunjed <cjmunjed@gmail.com>
2023-12-31 00:49:09 -0500

    Clean up code, revert sklearn to earlier version

--- api/api.py
+++ api/api.py
@@ -2,44 +2,25 @@ from flask import Flask, request, render_template
 #import sklearn
 import pickle
 import numpy as np
-#import logging
-#from pydantic import BaseModel
-#from flask_cors import CORS
 
-#from pathlib import Path
 import os
 import sys
-#import imp
 
-#BASE_DIR = Path(__file__).resolve(strict=True).parent
-#model_path = os.path.join(BASE_DIR, 'model', 'model.py')
-#m = imp.load_source("model", model_path)
-#model = m.model
-#model_version = imp.load_source("__version__", model_path)
-
-# Work-around for Vercel, add model's path to system path
+# Work-around for Vercel, add model to system path so it can be found
 if os.environ.get('VERCEL') == '1':
     sys.path.append(os.path.dirname(__file__))
 
 from model.model import model
 from model.model import __version__ as model_version
 
-print(os.listdir(os.getcwd()))
 
-#FRONT_END_URL = "https://tictactoe-seven-smoky.vercel.app/"
+build_path = '../dist'
+app = Flask(__name__, static_folder=build_path, template_folder=build_path, static_url_path='/')
 
-#p = os.path.join('..', os.path.basename(os.getcwd()))
-#p = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'dist')
-#print(p)
-
-#if os.environ.get('VERCEL') == '1':
-#    app = Flask(__name__, static_folder=p, template_folder=p, static_url_path='/')
-##else:
-app = Flask(__name__, static_folder='../dist', template_folder='../dist', static_url_path='/')
-#app = Flask(__name__)
-#CORS(app, origins=[FRONT_END_URL, "http://localhost:5173"])
 
 # To use and validate base models with routes, use Flask-Pydantic
+#from pydantic import BaseModel
+#from flask_cors import CORS
 #class BoardIn(BaseModel):
 #    board: str
 
@@ -49,7 +30,10 @@ app = Flask(__name__, static_folder='../dist', template_folder='../dist', static
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if os.path.exists(build_path):
+        return render_template('index.html')
+    else:
+        return {"error": "build not found, run 'npm run build' to serve front-end build from this route"}
 
 
 @app.route("/api/status")
